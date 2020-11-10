e65d00f664acad6ec4a5298315ca31a7202a0b58
thekhairul <thekhairul.office@gmail.com>
2020-11-10 21:32:36 +0600

    add transition to switch component

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -1,12 +1,26 @@
 import { useState } from "react";
 
-function Switch({bgColor,switchColor}) {
-    const [isSwitchOn, toggleSwitch] = useState(false);
-    return (
-        <div className={`flex ${isSwitchOn ? 'justify-end' : 'justify-start'} h-8 w-16 p-1 rounded-3xl ${bgColor || ''}`} onClick="">
-            <div className={`h-6 w-6 rounded-full ${switchColor || ''}`}></div>
-        </div>
-    );
+function Switch({ bgColor, switchColor }) {
+  const [isSwitchOn, setSwitch] = useState(false);
+
+  const toggleSwitch = () => {
+    setSwitch(!isSwitchOn);
+  };
+
+  return (
+    <div
+      className={`h-8 w-16 p-1 mx-2 rounded-3xl cursor-pointer ${
+        bgColor || ""
+      }`}
+      onClick={toggleSwitch}
+    >
+      <div
+        className={`duration-200 ease-in-out h-6 origin-center rounded-full transform transition-transform ${
+          isSwitchOn ? "translate-x-8" : ""
+        } w-6 ${switchColor || ""}`}
+      ></div>
+    </div>
+  );
 }
 
 export default Switch;
